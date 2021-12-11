8163a7f75ace3c3ab28bc8c42ea29bce1465f5d4
David McCullars <david.mccullars@gmail.com>
2021-12-11 00:46:06 -0600

    Improve solve to better support OO

--- solve.rb
+++ solve.rb
@@ -1,4 +1,5 @@
 require 'bundler'
+require 'singleton'
 Bundler.require
 
 # Kinda hacky, but we're going to capture the day when first loaded
@@ -8,77 +9,11 @@ year, day = $1, $2
 ENV['AOC_YEAR'] = year || Time.new.year.to_s
 AoC.define_singleton_method(:day) { day.to_i }
 
-def example(data, suffix)
-  case suffix
-  when :lines
-    data.lines(chomp: true)
-  when :text
-    data.chomp
-  when :numbers
-    data.lines.map(&:to_i)
-  when :number
-    data.lines.first.to_i
-  else
-    abort "Invalid example suffix: #{suffix.inspect}"
-  end
-end
-
-def input(suffix)
-  AoC.handle_name(AoC.get_year, "DAY#{AoC.day}_#{suffix}") or abort "Invalid suffix #{suffix.inspect}"
-end
-
-def solution_poster
-  @solution_poster ||= SolutionPoster.new
-end
-
-def solve(suffix = :lines, **examples)
-  @letter = @letter ? (@letter.ord + 1).chr : 'A'
-  puts "========================== #{@letter} =========================="
-
-  examples.each do |input, expected_result|
-    actual_result = yield example(input, suffix)
-    if expected_result != actual_result
-      abort "Expected result (#{expected_result}) does not equal actual result (#{actual_result.inspect})"
-    end
-  end
-
-  result = yield input(suffix)
-  puts "RESULT: #{result}", nil
-  solution_poster.post_solution(result)
-end
-
-%i[lines text numbers number].each do |suffix|
-  define_method("solve_with_#{suffix}") do |*args, **opts, &block|
-    solve(suffix, *args, **opts, &block)
-  end
-end
-
-def solve_with_commands(allowed_commands, *args, **opts)
-  solve(*args, **opts) do |lines|
-    commands = lines.map do |line|
-      if line =~ /^(#{allowed_commands.join('|')}) (\d+)$/
-        [$1, $2.to_i]
-      else
-        raise "Invalid: #{line.inspect}"
-      end
-    end
-    yield commands
-  end
-end
-
-def solve_with_line_of_numbers(*args, **opts, &block)
-  solve(:text, *args, **opts) do |text|
-    yield text.split(/\s*,\s*/).map(&:to_i)
-  end
-end
-
-def solve_with(clazz, *args, **opts)
-  solve(*args, **opts) do |data|
-    yield data.is_a?(Array) ? data.map { |d| clazz.new(d) } : clazz.new(data)
-  end
-end
+#########################################################################
 
 class SolutionPoster
+
+  include Singleton
 
   attr_reader :part
 
@@ -135,4 +70,81 @@ class SolutionPoster
     end
   end
 
+end
+
+#########################################################################
+
+def example(data, suffix)
+  case suffix
+  when :lines
+    data.lines(chomp: true)
+  when :text
+    data.chomp
+  when :numbers
+    data.lines.map(&:to_i)
+  when :number
+    data.lines.first.to_i
+  else
+    abort "Invalid example suffix: #{suffix.inspect}"
+  end
+end
+
+def input(suffix)
+  AoC.handle_name(AoC.get_year, "DAY#{AoC.day}_#{suffix}") or abort "Invalid suffix #{suffix.inspect}"
+end
+
+def classify(data, clazz)
+  clazz ? clazz.new(data) : data
+end
+
+def solve(suffix = :lines, clazz: nil, **examples)
+  @letter = @letter ? (@letter.ord + 1).chr : 'A'
+  puts "========================== #{@letter} =========================="
+
+  examples.each do |input, expected_result|
+    actual_result = yield classify(example(input, suffix), clazz)
+    if expected_result != actual_result
+      abort "Expected result (#{expected_result}) does not equal actual result (#{actual_result.inspect})"
+    end
+  end
+
+  result = yield classify(input(suffix), clazz)
+  puts "RESULT: #{result}", nil
+  SolutionPoster.instance.post_solution(result)
+end
+
+%i[lines text numbers number].each do |suffix|
+  define_method("solve_with_#{suffix}") do |**opts, &block|
+    solve(suffix, **opts, &block)
+  end
+end
+
+def solve_with_line_of_numbers(clazz: nil, **opts)
+  solve(:text, **opts) do |text|
+    yield classify(text.split(/\s*,\s*/).map(&:to_i), clazz)
+  end
+end
+
+def solve_with_grid_of_numbers(clazz: nil, **opts)
+  solve(**opts) do |lines|
+    grid = {}
+    lines.each_with_index do |line, row|
+      line.chars.each_with_index do |c, col|
+        grid[[row, col]] = c.to_i
+      end
+    end
+    yield classify(grid, clazz)
+  end
+end
+
+def solve_with(clazz, *args, **opts)
+  solve(*args, **opts) do |data|
+    yield classify(data, clazz)
+  end
+end
+
+def solve_with_each(clazz, *args, **opts)
+  solve(*args, **opts) do |data|
+    yield data.map { |d| classify(d, clazz) }
+  end
 end

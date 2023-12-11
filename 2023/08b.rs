46002946f431cc6d6455d61e0d6dd5606f8e2fec
David McCullars <david.mccullars@gmail.com>
2023-12-10 22:32:07 -0600

    2023-08

---
+++ 2023/08b.rs
@@ -0,0 +1,55 @@
+mod helpers;
+
+use std::collections::HashMap;
+use num::integer::lcm;
+use regex::*;
+use crate::helpers::*;
+
+fn to_id(s: &str) -> u16 {
+    s.chars().fold(0, |a, c| 26 * a + (c.to_ascii_lowercase() as u16) - 97)
+}
+
+fn main() {
+	let lines = input_lines(EXAMPLE);
+    let directions: Vec<char> = lines[0].chars().collect();
+
+    let re = Regex::new(r"(...) = \((...), (...)\)").unwrap();
+
+    let mut instructions: HashMap<u16, (u16, u16)> = HashMap::new();
+    for line in lines[2..].to_vec() {
+        let (_, [id, left, right]) = re.captures(line.as_str()).unwrap().extract();
+        instructions.insert(to_id(id), (to_id(left), to_id(right)));
+    }
+
+    let starts: Vec<&u16> = instructions.keys().filter(|id| *id % 26 == 0).collect();
+    let cycles: Vec<usize> = starts.into_iter().map(|start| {
+        let mut pos = *start;
+        let mut step = 0;
+        while pos % 26 < 25 {
+            let dir = directions[step % directions.len()];
+            pos = if dir == 'L' { instructions[&pos].0 } else { instructions[&pos].1 };
+            step += 1;
+            if step > 100000 {
+                eprintln!("Too many steps!");
+                std::process::exit(1);
+            }
+        }
+        step
+    }).collect();
+
+    let result = cycles.into_iter().fold(1, |a, b| lcm(a, b));
+	println!("{}", result);
+}
+
+const EXAMPLE: &str = "
+LR
+
+AAA = (AAB, XXX)
+AAB = (XXX, AAZ)
+AAZ = (AAB, XXX)
+BBA = (BBB, XXX)
+BBB = (BBC, BBC)
+BBC = (BBZ, BBZ)
+BBZ = (BBB, BBB)
+XXX = (XXX, XXX)
+";
